ae100d8ac156641226ed1f693fe3854b0d2752cc
Andrea Medeghini <andrea@nextbreakpoint.com>
2016-07-02 15:01:54 +0100

    Cleanup

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# fuzzylogic
+# FuzzyLogic 1.0.0
+
+FuzzyLogic implements a API for creating and executing a Fuzzy System.
+
+## Example
+
+Given the program:
+
+The output will be:
--- src/main/java/com/nextbreakpoint/fuzzylogic/FuzzyExpression.java
+++ src/main/java/com/nextbreakpoint/fuzzylogic/FuzzyExpression.java
@@ -2,9 +2,6 @@ package com.nextbreakpoint.fuzzylogic;
 
 import java.util.Map;
 
-/**
- * Created by medeghinia on 17/06/2016.
- */
 public interface FuzzyExpression {
     public FuzzyValue evaluate(Map<String, Double> inputs);
 }
